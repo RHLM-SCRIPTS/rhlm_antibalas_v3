97ea22c8b5a5ed31df8ae2a8036bbac7cfdef91b
Anuel AA <105551679+RHLM-SCRIPTS@users.noreply.github.com>
2022-06-24 01:24:23 +0200

    Add files via upload

---
+++ rhlm_antibalas_v3/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author "😈 𝐀𝐍𝐔𝐄𝐋 𝐀𝐀 😈#6979"
+
+discord "https://discord.gg/ZdDBjyYr9x"
+
+fxversion "3.0"
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',	
+	'server/*.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'client/*.lua'
+}
